e8a533a090db3b4aa00c42ffe27195abe2bc5f58
bp442 <bpark135@gmail.com>
2023-10-17 14:23:34 -0400

    finished exercises on 2-13

---
+++ MathScripts/area_of_circle.js
@@ -0,0 +1,9 @@
+//How do you calculate the area of a circle?
+//sample radius is 5
+
+var radius = 5;
+var pi = 3.14159;
+
+var area = pi * Math.pow(radius, 2);
+
+console.log("The area of a circle with radius " + radius + " is " + area);
