4b238eeb2e86e41d8ac1ae3f64ab4573d858bfff
bp442 <bpark135@gmail.com>
2023-10-17 14:11:36 -0400

    demos and added folder for workbook 2 exercises

---
+++ demo3.js
@@ -0,0 +1,32 @@
+//Exercises on page 1-38
+
+/*
+Exercise 1
+Use var to create variables with good names for the following...
+*/
+var customerID;
+var customerFirst; var customerMiddle; var customerLast;
+var customerGender;
+var customerDOB;
+var customerLicenseNum;
+var customerPolicyNum;
+
+/*Exercise 2
+Names that you either can not, or should not use for variable names.
+
+1. arguments, boolean, break, catch, char, const, double, short, static, int, true
+2. Array, date, eval, function, hasOwnProperty,Infinity, length, Math, Object,String
+3. alert, assign, embed, element, event, focus, link, navigator, hidden, setInterval
+4. onblur, onclick, onerror, onfocus, onkeydown, onkeypress, onkeyup
+*/
+
+/*
+Exercise 3
+Write JavaScript to declare variables for your name, as well as the city and state you were born in. Then assign those variables values that represent attributes of yourself!
+*/
+var firstName, lastName, cityOfBirth, stateOfBirth;
+
+firstName= "Brian";
+lastName= "Park";
+cityOfBirth= "Long Beach";
+stateOfBirth= "California";
---
+++ tips.js
@@ -0,0 +1,11 @@
+// sample inputs
+var foodCost = 79.25;
+var tax = 6.54;
+var tip = 12.00;
+// calculations
+var totalDue = foodCost + tax + tip;
+// output
+console.log("The food cost is $" + foodCost + " and the tax is $" + tax);
+console.log("The tip is $" + tip.toFixed(2));
+console.log("Total Due is " + totalDue);
+
