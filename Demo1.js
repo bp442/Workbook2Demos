b19e96653a9095a8884ec0d78482d11170a4dba4
bp442 <bpark135@gmail.com>
2023-10-17 13:04:59 -0400

    done with demo1

---
+++ Demo1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var firstName;
+var lastName;
+
+// assign a value "Brian" to the variable labeled firstName
+firstName = "Brian";
+
+// lastName has the value of "Park"
+lastName = "Park";
+
+var fullName;
+
+fullName = lastName + ", " + firstName;
+
+var message;
+
+message = "The user's full name is: " + fullName;
+
+
+console.log(message);
