b42199a9d7febcf903502f9f0e0184a9c2e8b693
bp442 <bpark135@gmail.com>
2023-10-19 13:16:56 -0400

    added demo10

---
+++ Demo10.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var age = 16;
+var parentPresent = true;
+
+if
+    ((age <= 18 && parentPresent)
+    ||
+    (age < 62 && age > 18)
+    )
+{
+
+}
--- MathScripts/area_of_circle.js
+++ MathScripts/area_of_circle.js
@@ -2,8 +2,7 @@
 //sample radius is 5
 
 var radius = 5;
-var pi = 3.14159;
 
-var area = pi * Math.pow(radius, 2);
+var area = Math.PI * Math.pow(radius, 2);
 
 console.log("The area of a circle with radius " + radius + " is " + area);
